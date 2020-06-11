6657d64f3abd66fac279d395d48ef9281dd846ca
phurpatshering79 <phurpa.tshering79@gmail.com>
2020-06-11 01:21:01 -0400

    fixing typos

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(express.static('website'));
 
 //set the port number
-const port = 8000
+const port = process.env.portcl || 8080
 
 //create a new varible that is the server, using the .listen() method on the app instance.
 //It accepts two arguments, one is the port and the other is a callback functioin that initiates the action once the server is 
